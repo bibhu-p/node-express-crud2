b5d7a3857082de07a9ae8a80618a9b1f8b82644d
Bibhu Prasad <bibhup.behera@nettantra.net>
2022-04-26 18:36:37 +0530

    Small changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,6 +10,6 @@ route.post('/login',userController.login);
 route.get('/find',middleware,userController.find);
 route.get('/find/:id', userController.findById);
 route.put('/update/:id',userController.update);
-route.delete('/delete',userController.delete);
+route.delete('/delete/:id',userController.delete);
 
 module.exports = route
